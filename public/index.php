a06c11073e25d3ddeca84ed9dcc4ae0d7349fca3
Roman Nadein <rnik82@mail.ru>
2024-11-08 16:21:14 +0300

    update index.php

--- public/index.php
+++ public/index.php
@@ -5,13 +5,32 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 
+//$app->addErrorMiddleware(true, true, true);
+
+// return $response->getBody()->write('Welcome to Slim!');
+// Благодаря пакету slim/http этот же код можно записать короче
+// return $response->write('Welcome to Slim!');
+
 $app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+    return $response->write('GET /');
 });
+
+$app->get('/users', function ($request, $response) {
+  return $response->write('GET /users');
+});
+
+$app->get('/companies', function ($request, $response) {
+    return $response->write('GET /companies');
+});
+
+$app->post('/companies', function ($request, $response) {
+    return $response->write('POST /companies');
+});
+
+$app->post('/users', function ($request, $response) {
+  return $response->withStatus(302);
+});
+
 $app->run();
