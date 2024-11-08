2d0ceba0e4f0d4595df92abdf2f7b7067211d39f
Roman Nadein <rnik82@mail.ru>
2024-11-08 14:31:03 +0300

    init test-slim

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+$name = 'Lessons';
+?>
+
+<div>
+  <a href="/lessons/<?= 23 ?>"><?= $name ?></a>
+</div>
+
+// Печатает год в STDOUT
+// header('Cache-control: private, max-age=0');
+// echo date('Y');
+// echo "<br>";
+// var_dump($_POST);
+// echo "<br>";
+// echo 'Hello, world!____';
+
+//header('Location: http://www.example.com/');
